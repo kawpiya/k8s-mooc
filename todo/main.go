0415e6504def58bf8a189a17390944bc57a40692
kaushalyap <24698778+kaushalyap@users.noreply.github.com>
2026-01-14 15:49:37 +0530

    Added root endpoint and exposed a port in todo app

--- todo/main.go
+++ todo/main.go
@@ -12,7 +12,10 @@ func main() {
 		port = "9090"
 	}
 	addr := ":" + port
-
 	log.Printf("Starting server on %s\n", addr)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
